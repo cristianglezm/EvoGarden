ffa10ba7f862cd2380a7e3d9706c9215f2db3e01
Cristian Gonzalez <cristian.glez.m@gmail.com>
2025-10-14 16:43:53 +0200

    fix(logo-path): fix logo base url

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Logo: React.FC = () => {
     return (
         <div className="flex flex-col items-center flex-shrink-0">
-            <img src="/EvoGarden-logo.png" title="EvoGarden" alt="EvoGarden Logo" className="h-18" />
+            <img src={`${import.meta.env.BASE_URL}EvoGarden-logo.png`} title="EvoGarden" alt="EvoGarden Logo" className="h-18" />
             <p className="text-xs text-secondary/70 font-mono -mt-1" aria-label={`Version ${import.meta.env.VITE_APP_VERSION}`}>
                 v{import.meta.env.VITE_APP_VERSION}
             </p>
